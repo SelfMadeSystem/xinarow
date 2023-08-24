d726a380b91b32a69e08683d67fa4a0613c72b33
SelfMadeSystem <sms@shoghisimon.cc>
2023-08-24 17:05:13 -0400

    Hide online stuff for now kuz I wanna put this on cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
 import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
 import socket from './src/server/socket'
 
 const port = Math.floor(Math.random() * 1000) + 3000
 
 export default defineConfig({
-    // plugins: [mkcert()],
     root: "src/client/",
     base: '',
     
     server: {
         port: 3001,
         https: false,
-        proxy: {
-            '/socket.io': {
-                target: `ws://localhost:${port}`,
-                changeOrigin: true,
-                ws: true,
-            },
-        },
+        // proxy: {
+        //     '/socket.io': {
+        //         target: `ws://localhost:${port}`,
+        //         changeOrigin: true,
+        //         ws: true,
+        //     },
+        // },
     },
     build: {
         outDir: "../../dist/",
@@ -40,4 +38,4 @@ export default defineConfig({
     appType: 'spa',
 })
 
-socket(port)
+// socket(port)
